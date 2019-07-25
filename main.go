a4615420e643622c9d923ff7ac550dcefa788161
oyakata <oyakata@users.noreply.github.com>
2019-07-25 09:43:17 +0900

    乱数生成器が固定値を返す場合、暗号化の結果も固定されるのか確かめた

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	baseValue := "1234-abcd-5678-efgh"
+	baseValue := "1234-	abcd-56\r78-\nefgh"
 
 	value, err := seckeyenc.Encrypt([]byte(baseValue))
 	fmt.Println(string(value), err)
@@ -20,4 +20,8 @@ func main() {
 
 	b, err := seckeyenc.DecryptBase64URLString(s)
 	fmt.Println("Base64URL:", string(b), err)
+
+	fmt.Println("=========================")
+
+	seckeyenc.Hello("hello-world-0123")
 }
--- seckeyenc/seckeyenc.go
+++ seckeyenc/seckeyenc.go
@@ -5,15 +5,23 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/base64"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/url"
 )
 
 var (
-	pubKey *rsa.PublicKey
-	priKey *rsa.PrivateKey
+	pubKey      *rsa.PublicKey
+	priKey      *rsa.PrivateKey
+	dummyReader = &DummyReader{}
 )
+
+type DummyReader struct{}
+
+func (d *DummyReader) Read(p []byte) (int, error) {
+	return 1, nil
+}
 
 func init() {
 	b, err := ioutil.ReadFile("seckeyenc/sample.der")
@@ -66,4 +74,25 @@ func DecryptBase64URLString(s string) ([]byte, error) {
 	}
 
 	return Decrypt(msg)
+}
+
+func Hello(value string) {
+	msg := []byte(value)
+	b, _ := rsa.EncryptPKCS1v15(rand.Reader, pubKey, msg)
+	enc := url.QueryEscape(base64.StdEncoding.EncodeToString(b))
+
+	b, _ = rsa.EncryptPKCS1v15(dummyReader, pubKey, msg)
+	enc2 := url.QueryEscape(base64.StdEncoding.EncodeToString(b))
+
+	fmt.Println("rand.Reader:", enc)
+	fmt.Println("dummyReader:", enc2)
+
+	esc, _ := url.QueryUnescape(enc)
+	msg, _ = base64.StdEncoding.DecodeString(esc)
+
+	dec, _ := rsa.DecryptPKCS1v15(rand.Reader, priKey, msg)
+	dec2, _ := rsa.DecryptPKCS1v15(dummyReader, priKey, msg)
+
+	fmt.Println("rand.Reader: decode:", string(dec))
+	fmt.Println("dummyReader: decode:", string(dec2))
 }
