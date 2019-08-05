1701d822f58841e2b19ddead5e96708c9c9ee9c2
oyakata <oyakata@users.noreply.github.com>
2019-08-05 18:56:13 +0900

    不思議なScanのやりかけ: うごきません

---
+++ scan_no_nazo/fixture/mysql/init/foo.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `students` (
+  id INT PRIMARY KEY,
+  active TINYINT NULL,
+  name VARCHAR(255) NULL,
+  grade INT NULL,
+  score DOUBLE NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `students` (id, active, name, grade, score) VALUES
+  (1, 1, 'John Doe', 65535, 0.009876),
+  (2, NULL, NULL, NULL, NULL)
+;
